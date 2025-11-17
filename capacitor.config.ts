a52d6c636a82adb4f4451ced17b906355b9251d7
Manish Joshi <manish.joshi@sutara.org>
2025-11-17 11:43:32 +0530

    added capacitor android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,16 @@
-/// <reference types="vite/client" />
 import type { CapacitorConfig } from '@capacitor/cli';
+import * as dotenv from "dotenv";
 
+dotenv.config();
+
+console.log("process.env.VITE_LIVE_UPDATE_APP_ID:😍😍😍 ", process.env.VITE_CAPACITOR_HOT_UPDATE_APP_ID);
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'capawesome-live-update-demo',
   webDir: 'dist',
   plugins: {
     LiveUpdate: {
-      appId: import.meta.env.VITE_LIVE_UPDATE_APP_ID,
+      appId: process.env.VITE_CAPACITOR_HOT_UPDATE_APP_ID,
       autoDeleteBundles: true,
       readyTimeout: 10000,
     }
