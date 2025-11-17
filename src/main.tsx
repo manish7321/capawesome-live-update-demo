874c9fb67461ab085e6f65c83322b9f15ea60e9b
Manish Joshi <manish.joshi@sutara.org>
2025-11-17 12:04:58 +0530

    setting up github actions for build and semi-automated hot update

--- src/main.tsx
+++ src/main.tsx
@@ -4,43 +4,37 @@ import './index.css'
 import App from './App.tsx'
 import { Capacitor } from '@capacitor/core'
 import { LiveUpdate } from '@capawesome/capacitor-live-update'
+import { SplashScreen } from '@capacitor/splash-screen'
 
 async function checkForUpdate() {
   try {
     if (Capacitor.isNativePlatform()) {
       await LiveUpdate.ready();
       console.log("LiveUpdate is ready🚀");
+      const { versionName } = await LiveUpdate.getVersionName();
+      const majorVersion = versionName.split(".")[0];
+      console.log("majorVersion: 👮‍♀️👮", majorVersion);
       const { bundleId: currentBundleId } = await LiveUpdate.getCurrentBundle();
-      const result = await LiveUpdate.fetchLatestBundle({ channel: `test-1` });
-      console.log("LiveUpdate fetchbundle result 💁", result);
+      const result = await LiveUpdate.fetchLatestBundle({ channel: `${import.meta.env.REACT_APP_ENV}-${majorVersion}` });
+      console.log("LiveUpdate fetch latest bundle result 💁", result);
       if (result.bundleId && result.downloadUrl && currentBundleId !== result.bundleId) {
-        const acceptUpdate = window.confirm('A new update is available. Would you like to install it?');
+        await SplashScreen.hide();
+        const acceptUpdate =window.confirm("A new update is available. Do you want to download and install it now?");
         if (acceptUpdate) {
+          await SplashScreen.show({ autoHide: false });
           const downloadStart = performance.now();
-          await LiveUpdate.downloadBundle({
-            bundleId: result.bundleId,
-            url: result.downloadUrl,
-          });
-          const downloadEnd = performance.now();
-          console.log(`⬇️ Bundle download: ${(downloadEnd - downloadStart).toFixed(2)} ms`);
-
-          const setStart = performance.now();
-          await LiveUpdate.setNextBundle({ bundleId: result.bundleId });
-          const setEnd = performance.now();
-          console.log(`📦 Set next bundle: ${(setEnd - setStart).toFixed(2)} ms`);
-
-          const reloadStart = performance.now();
+          await LiveUpdate.sync({channel: `${import.meta.env.REACT_APP_ENV}-${majorVersion}`});
           await LiveUpdate.reload();
-          const reloadEnd = performance.now();
-          console.log(`🔄 Reload time: ${(reloadEnd - reloadStart).toFixed(2)} ms`);
-
-          const totalEnd = reloadEnd; 
+          const totalEnd = performance.now(); 
+          console.log(`🚀 LiveUpdate: Update applied successfully to bundle ${result.bundleId}`);
           console.log(`⏱️ Total time from confirmation to reload: ${(totalEnd - downloadStart).toFixed(2)} ms`);
         }
       }
     }
   } catch (err) {
-    console.warn("LiveUpdate.sync() failed", err);
+    console.error("LiveUpdate failed❌", err);
+  } finally {
+    await SplashScreen.hide();
   }
 }
 
